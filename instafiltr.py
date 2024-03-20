06e9a6a21753470be0b4260cd7748ec0550b3b3a
janleitgeb <janleitgeb05@gmail.com>
2024-03-20 12:00:24 +0100

    snizeni highlitu a snizeni kontrastu

--- instafiltr.py
+++ instafiltr.py
@@ -16,19 +16,18 @@ sirka, vyska = obrazek.size
 for x in range(sirka):
     for y in range(vyska):
         r, g, b = obrazek.getpixel((x, y))
-        if volba == "cernobily":
-            prumer = int((r + g + b) / 3)
-            obrazek.putpixel((x, y), (prumer, prumer, prumer))
-        elif volba == "saturovanejsi":
-            
-            r = min(255, int(r * 1.6))  #zvyseni saturace
-            g = min(255, int(g * 1.6))  
-            b = min(255, int(b * 1.6))  
 
-            r = int(r * 0.7)  #snizeni highlitu
-            g = int(g * 0.7)  
-            b = int(b * 0.7)  
-            obrazek.putpixel((x, y), (r, g, b))
+        # Zvýšení saturace
+        r = min(255, int(r * 1.6))  
+        g = min(255, int(g * 1.6))  
+        b = min(255, int(b * 1.6))  
+
+        # Snížení highlitů
+        r = int(r * 0.6)  
+        g = int(g * 0.6)  
+        b = int(b * 0.6)
+
+        obrazek.putpixel((x, y), (r, g, b))  
 
 obrazek.show()
 
