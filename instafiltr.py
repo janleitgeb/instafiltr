6f24816a6e9edd93ec8637bc6ca03019abbd4028
janleitgeb <janleitgeb05@gmail.com>
2024-03-20 11:55:54 +0100

    snizeni highlitu a uprava saturace

--- instafiltr.py
+++ instafiltr.py
@@ -1,16 +1,16 @@
 from PIL import Image
 
-print("vitej v programu pro upravu obrazku!")
+print("vitej na upravvu!")
 print("========================================")
 
-nazev_souboru = input("zadej nazev souboru s obrazkem (+ pripona): ")
+nazev_souboru = input("zadej nazev souboru + pripona: ")
 try:
     obrazek = Image.open(nazev_souboru)
 except FileNotFoundError:
-    print("neexistuje pico.")
+    print("spatne napsany, demente.")
     quit()
 
-volba = input("jak chcete upravit obrazek? (cernobily / saturovanejsi): ")
+volba = input("jak to chces upravit? (cernobily / saturovanejsi): ")
 
 sirka, vyska = obrazek.size
 for x in range(sirka):
@@ -20,12 +20,15 @@ for x in range(sirka):
             prumer = int((r + g + b) / 3)
             obrazek.putpixel((x, y), (prumer, prumer, prumer))
         elif volba == "saturovanejsi":
-            r = min(255, int(r * 1.5))
-            g = min(255, int(g * 1.5))
-            b = min(255, int(b * 1.5))
-            r = int(r * 0.8)
-            g = int(g * 0.8)
-            b = int(b * 0.8)
+            
+            r = min(255, int(r * 1.6))  #zvyseni saturace
+            g = min(255, int(g * 1.6))  
+            b = min(255, int(b * 1.6))  
+
+            r = int(r * 0.7)  #snizeni highlitu
+            g = int(g * 0.7)  
+            b = int(b * 0.7)  
             obrazek.putpixel((x, y), (r, g, b))
 
 obrazek.show()
+
